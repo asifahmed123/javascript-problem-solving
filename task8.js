16898371e135e09ba4ed7a9da7c0cd5bdd4b8af2
asifahmed123 <ahmedasif092@gmail.com>
2023-07-27 17:05:35 +0600

    second commit

--- task8.js
+++ task8.js
@@ -1,7 +1,17 @@
 // Implement a JavaScript function to find the second smallest element in an array of numbers. The function should return the second smallest number.
 
 const secondSmallest = (num) => {
-
+     let smallest = Infinity;
+     let secondSmallest = Infinity;
+     for (let i = 0; i < num.length; i++) {
+          if(num[i] < smallest){
+               secondSmallest = smallest;
+               smallest = num[i]
+          }
+          else if(num[i] < secondSmallest && num[i] !== smallest){
+               secondSmallest = num[i];
+          }
+     }
+     return secondSmallest;
 }
-
-secondSmallest([19,30,190,20,2,56,60])
+console.log(secondSmallest([19, 30, 190, 20, 2, 56, 60]));
